0a390bb0cc34ccd7ac7dafb659dda6163fbe62f1
SafyaAbouelfadel <safia.abf@gmail.com>
2025-02-09 14:19:37 +0000

    start a Flask web application

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""an Flask appilication that great user"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """
+    Greet user with 'Hello HBNB' message
+
+    Returns:
+        str: A greeting message
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello():
+    """
+    Greet user with 'HBNB' message
+
+    Returns:
+        str: A greeting message
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    """
+    Route that displays 'C ' followed by the value of 'text'.
+
+    Args:
+        text (str); The input message.
+
+    Returns:
+        str: A formatted message.
+    """
+    return "C {}".format(text.replace("_"," "))
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def python_route(text):
+    """
+    Route that displays 'Python ' followed by the value of 'text'.
+
+    Args:
+        text (str); The input message.
+
+    Returns:
+        str: A formatted message.
+    """
+    return "Python {}".format(text.replace("_"," "))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
